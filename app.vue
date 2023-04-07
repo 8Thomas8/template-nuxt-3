0d5d15cc6ac46ac5e91263b952328411c500730e
Thomas Rousseau <thomas.rousseau@proton.me>
2023-04-07 21:37:16 +0200

    Ajout de config d'example Meta, Head, SEO

--- app.vue
+++ app.vue
@@ -1,3 +1,37 @@
+
+<script setup lang="ts">
+// Todo Changer les fonts si besoin
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossorigin: 'anonymous'
+    },
+    {
+      href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700;800&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap',
+      rel: 'stylesheet'
+    }
+  ]
+})
+
+// TODO Config ici
+useSchemaOrg([
+  defineOrganization({
+    name: 'Nom de l\'organisation',
+    logo: '/images/logo/example.svg'
+  }),
+  defineWebSite({
+    name: 'Nom du site'
+  }),
+  defineWebPage()
+])
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+defineOgImageScreenshot({
+  // TODO Mettre à jour l'alt de l'image illustration SEO
+  alt: 'Home page'
+})
+useSeoMeta({
+  // TODO Mettre à jour le titre SEO
+  title: 'Home',
+  // TODO Mettre à jour la description SEO
+  description: 'Home page'
+})
+useHead({
+  link: [
+    // TODO Mettre à jour l'url canonical
+    {
+      rel: 'canonical',
+      href: 'https://www.example.com/'
+    }
+  ]
+})
+</script>
+
 <template>
   <div />
 </template>
