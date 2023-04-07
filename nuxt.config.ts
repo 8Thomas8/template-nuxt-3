de90cf11491b3a5c591c61d20a959ae1d449a4b5
Thomas Rousseau <thomas.rousseau@proton.me>
2023-04-07 21:24:50 +0200

    Ajout libs + pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module', 'nuxt-og-image', 'nuxt-schema-org'],
+    css: [
+        '@/assets/scss/main.scss'
+    ],
+    tailwindcss: {
+        cssPath: '~/assets/scss/main.scss',
+        configPath: 'tailwind.config',
+        exposeConfig: false,
+        config: {
+            content: [],
+            theme: {
+                fontFamily: {
+                    poppins: ['"Poppins"', 'sans-serif'],
+                    roboto: ['"Roboto"', 'sans-serif']
+                }
+            },
+            plugins: [
+                require('@tailwindcss/typography')
+            ]
+        },
+        injectPosition: 0,
+        viewer: true
+    },
+    eslint: {
+        lintOnStart: false,
+        emitWarning: true,
+        emitError: true,
+        failOnError: true,
+        failOnWarning: false
+    },
+    schemaOrg: {
+        // TODO Changer url
+        host: 'https://exemple.fr/'
+    }
 })
